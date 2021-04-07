6865fd4ac773a11fc548c5794f38d527a7135b9a
Julliah Sothiraj <julliahsothiraj@gmail.com>
2021-04-07 15:13:48 +0200

    ajout du front

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,7 @@ Encore
      */
     .addEntry('app', './assets/app.js')
     .addEntry('login', './assets/styles/login.css')
+    .addEntry('home', './assets/styles/home.css')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
